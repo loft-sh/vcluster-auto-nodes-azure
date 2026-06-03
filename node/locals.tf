50348f515f1389871769431b62838e0ec0f36fcb
Josef Mende <josef.mende@vcluster.com>
2026-06-03 16:55:09 +0200

    Fix: Support node module for non-Node Machines

--- node/locals.tf
+++ node/locals.tf
@@ -1,11 +1,8 @@
 locals {
-  vcluster_name      = nonsensitive(var.vcluster.instance.metadata.name)
-  vcluster_namespace = nonsensitive(var.vcluster.instance.metadata.namespace)
-
   location            = nonsensitive(module.validation.location)
   resource_group_name = nonsensitive(module.validation.resource_group)
 
-  vm_name       = format("%s-%s", local.vcluster_name, random_id.vm_suffix.hex)
+  vm_name       = format("%s-%s", var.vcluster.name, random_id.vm_suffix.hex)
   instance_type = nonsensitive(var.vcluster.nodeType.spec.properties["instance-type"])
 
   vnet_id           = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["vnet_id"])
